2217b6eb26620ea83f78512768407fde1d782536
My Name <knuckostya@gmail.com>
2023-05-17 17:47:58 +0300

    HW 10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,4 @@
 import s from './Loader.module.css'
+import loader from './Spinner-1s-200px (1).gif'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}><img src={loader} alt=""/></div>
