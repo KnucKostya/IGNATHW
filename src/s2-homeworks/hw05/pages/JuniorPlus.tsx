fff0db5a2dd3acae65e4c48e18105bfc2db766f2
My Name <knuckostya@gmail.com>
2023-05-22 17:27:05 +0300

    HW 14 try

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 // import HW11 from '../../hw11/HW11'
 // import HW12 from '../../hw12/HW12'
 import HW13 from '../../hw13/HW13'
+import HW14 from "../../hw14/HW14";
 
 function JuniorPlus() {
     return (
@@ -10,7 +11,9 @@ function JuniorPlus() {
             {/*<HW10 />*/}
             {/*<HW11 />*/}
             {/*<HW12 />*/}
-            <HW13 />
+            {/*<HW13 />*/}
+            <HW14 />
+            {/*<HW13 />*/}
         </div>
     )
 }
