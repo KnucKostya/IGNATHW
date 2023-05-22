4ba5a7124d10451a06263a7166528d45340dedd6
My Name <knuckostya@gmail.com>
2023-05-22 18:43:59 +0300

    HW 14

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -33,11 +33,13 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
 
         if (onDebouncedChange) {
             // делает студент
-            setTimerId(0)
-            setTimerId(1500)
+            clearTimeout(timerId)
+            const newTimerId = setTimeout(() => {
+                onDebouncedChange(value)
+            }, 1500)
+            setTimerId(+newTimerId)
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
-            onDebouncedChange(value)
             //
         }
     }
